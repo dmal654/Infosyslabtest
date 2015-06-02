4169e795b2da92556193e6aae84d2f46ca98744d
Ben <dmal654@aucklanduni.ac.nz>
2015-06-02 21:56:34 +0000

    DONe

---
+++ javascript.js
@@ -0,0 +1,11 @@
+/*function toggle(){
+	var on = "./images/turnon.png";
+	var off = "./images/turnoff.png";
+	var logo1 = document.getElementById("log");
+	var logo2 = document.getElementById("log1");
+	
+	var cond = false;
+	
+	prompt("CLICK");
+
+}*/
